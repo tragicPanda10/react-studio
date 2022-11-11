fa89a3daf51cd57ab122d4bd1cf86281b0a97ef6
tragicPanda10 <114897663+tragicPanda10@users.noreply.github.com>
2022-11-11 17:34:00 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import menuItem from "./components/BakeryItem.js";
+import  Button from '@mui/material/Button';
+import { Grid } from "@mui/material";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -9,20 +12,71 @@ bakeryData.forEach((item) => {
 /* ############################################################## */
 
 function App() {
-  // TODO: use useState to create a state variable to hold the state of the cart
-  /* add your cart state code here */
+  const [cart, addCart] = useState([]);
+  const [total, addTotal] = useState(0);
+  
+  function handleCart(item) {
+      addTotal(total + item.price);
+      addCart([...cart, {
+        "name": item.name,
+        "quantity": 1
+      },]);
+  }
 
+function sumCart() {
+  let sum=[];
+  for(let i =0; i<cart.length; i++) {
+    if(sum.includes(cart[i])) {
+      sum[sum.indexOf(cart[i])] = {
+        "name": cart[i].name,
+        "quantity": cart[i].quantity + 1
+      };
+    }
+    else {
+      sum = [...sum, cart[i], ]
+    }
+  }
+
+  return(sum);
+}
+
+ function dispCart() {
+  if(cart.length !==0) {
+    return(
+    <div class="cart-items">
+      {sumCart().map((element) => {
+        return(
+        <div>
+          {element.name} x{element.quantity}
+        </div>);
+      })}
+    </div>
+    );}
+  }
+  
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <div class="main-body">
+        <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+        <Grid container rowSpacing={1} columnSpacing={1}>
+          {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+          <Grid item xs={6}>
+            <div class="card">
+              {menuItem(index, bakeryData)}
+              <Button size="small" variant="contained" color="secondary" onClick={() => {handleCart(item)}}>Add to cart</Button>
+            </div> 
+          </Grid>
+          
+        ))}</Grid>
+      </div>
+      
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
-      <div>
+      <div class="cart">
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+          {dispCart()}
+        <div id="total">
+          Total: ${total.toFixed(2)}
+        </div>
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,20 @@
 // TODO: create a component that displays a single bakery item
+
+export default function menuItem(index, arr) {
+    return(
+        <div>
+            <div class="fruit">
+                {arr[index].name}
+            </div>
+            <div class="price">
+               ${arr[index].price}
+            </div>
+            <div>
+                <img src={arr[index].image} alt='fruit'></img>
+            </div>
+            <div class="desc">
+                {arr[index].description}
+            </div>
+        </div>
+    );
+}
